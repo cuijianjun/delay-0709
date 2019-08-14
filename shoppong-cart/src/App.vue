1748f4f68a00476b4cbd246aac6d52fa0aed11d3
Frank <376247934@qq.com>
2019-08-14 10:45:02 +0800

    $2

---
+++ shoppong-cart/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <template>
+      <k-input></k-input>
+      <div>
+        <h3>Element表单</h3>
+        <hr>
+        <el-form :model="model" :rules="rules" ref="loginForm">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="model.username" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="password">
+            <el-input type="password" v-model="model.password" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import KInput from './components/Input';
+export default {
+  name: 'app',
+  components: {
+    KInput
+  },
+  data() {
+    return {
+      value:'',
+      model: { username: "tom", password: "" },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名" },
+          {min: 6,max:10,message:'请输入6~10的用户名'}
+        ],
+        password: [
+          { required: true, message: "请输入密码" }
+        ],
+      }
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ shoppong-cart/src/components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="onInput">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Input',
+  props:['value'],
+  methods: {
+    onInput(e) {
+      let value = e.target.value;
+      this.$emit('input')
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
